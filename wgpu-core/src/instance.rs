bb25bb02bfa5f4c7917781a49e3100fcff541882
Florian Denis <florian@photoroom.com>
2025-07-18 13:38:36 +0200

    Revert "debug for gles loading"
    This reverts commit 840db1ab59da3c91a6a086202704dedfc14e5975.

--- wgpu-core/src/instance.rs
+++ wgpu-core/src/instance.rs
@@ -134,7 +134,7 @@ impl Instance {
                     .push((A::VARIANT, Box::new(instance)));
             }
             Err(err) => {
-                log::error!(
+                log::debug!(
                     "Instance::new: failed to create {:?} backend: {:?}",
                     A::VARIANT,
                     err
--- wgpu-hal/src/gles/egl.rs
+++ wgpu-hal/src/gles/egl.rs
@@ -868,8 +868,6 @@ impl crate::Instance for Instance {
         let egl = match egl_result {
             Ok(egl) => Arc::new(egl),
             Err(e) => {
-                let test_1_3 = unsafe { khronos_egl::DynamicInstance::<khronos_egl::EGL1_3>::load_required() }.map(|_| ());
-                log::error!("1.3: {:?}", test_1_3);
                 return Err(crate::InstanceError::with_source(
                     String::from("unable to open libEGL"),
                     e,
@@ -881,10 +879,7 @@ impl crate::Instance for Instance {
 
         let client_ext_str = match client_extensions {
             Ok(ext) => ext.to_string_lossy().into_owned(),
-            Err(err) => {
-                log::error!("Failed to query EGL client extensions: {err}");
-                String::new()
-            },
+            Err(_) => String::new(),
         };
         log::debug!(
             "Client extensions: {:#?}",
